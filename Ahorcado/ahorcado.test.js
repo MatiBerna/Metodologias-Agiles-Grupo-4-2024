704ad99784caf0ad050031a617bf49acaab50baa
Matias Bernardi <matias.bernardi.1@gmail.com>
2024-06-03 21:27:13 -0300

    tests necesarios para el sprint

--- Ahorcado/ahorcado.test.js
+++ Ahorcado/ahorcado.test.js
@@ -20,4 +20,34 @@ describe('Ahorcado', () => {
     const result = miAhorcado.setNombre(nombre)
     expect(result).toThrowError(/^Debe ingresar un nombre valido$/)
   })
+  test('Arriesgar letra, es incorrecta y pierde 1 vida', () => {
+    const letra = 'd'
+
+    const result = miAhorcado.arriesgarLetra(letra)
+
+    expect(result).toBe('Letra incorrecta, intentos restantes: 6')
+  })
+
+  test('Arriesgar letra y es correcta', () => {
+    const letra = 'a'
+    const result = miAhorcado.arriesgarLetra(letra)
+    expect(result).toBe('Letra correcta')
+  })
+
+  test('Arriesgar palabra y es incorrecta', () => {
+    const palabra = 'arriesgar'
+    const result = miAhorcado.arriesgarPalabra(palabra)
+    expect(result).toBe('Palabra incorrecta, intentos restantes: 5')
+  })
+
+  test('Arriesgar palabra y es correcta', () => {
+    const palabra = 'arriesgar'
+    const result = miAhorcado.arriesgarPalabra(palabra)
+    expect(result).toBe('GANA')
+  })
+
+  test('Mostar vidas restantes', () => {
+    const result = miAhorcado.devuelveVidas()
+    expect(result).toBe(7)
+  })
 })
